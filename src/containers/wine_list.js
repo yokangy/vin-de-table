3c66c0bac0a415923d7c56d154c419dd85eb8456
Yondy Kang <yokangy@gmail.com>
2016-04-27 21:59:17 -0700

    Created component for displaying list of wines.

---
+++ src/containers/wine_list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class WineList extends Component {
+  render() {
+    return (
+      <div>
+        List of Wines
+      </div>
+    );
+  }
+}
