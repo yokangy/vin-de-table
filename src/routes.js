fa7766a8e51f3b370af7d189b7590d3dd12021cb
Yondy Kang <yokangy@gmail.com>
2016-04-27 22:01:15 -0700

    Added index route and have it display the wine list.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import CreateWineReview from './containers/wine_review_create';
+import WineList from './containers/wine_list';
 
 export default (
 	<Route path="/" component={App}>
+    <IndexRoute component={WineList} />
 	</Route>
 );
