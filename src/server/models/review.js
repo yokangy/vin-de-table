da77c6dda744aec11ac1f0d08ad60cde9452f2eb
Yondy Kang <yokangy@gmail.com>
2016-07-02 13:10:30 -0700

    Removed user field from reviews model. User will not be MVP.

--- src/server/models/review.js
+++ src/server/models/review.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const reviewsSchema = new Schema({
   rating: Number,
   review: String,
-  wine: { type: Schema.Types.ObjectId, ref: 'wines' }
-  user: { type: Schema.Types.ObjectId, ref: 'users' }
+  wine: { type: Schema.Types.ObjectId, ref: 'wines' },
+  //user: { type: Schema.Types.ObjectId, ref: 'users' }
 });
 
 const ModelClass = mongoose.model('reviews', reviewsSchema);
