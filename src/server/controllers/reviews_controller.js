3596ea503a036b137a793035d0be0bc3035fe2f7
Yondy Kang <yokangy@gmail.com>
2016-07-02 13:07:02 -0700

    Created scaffolding for methods to retrive and create reviews.

---
+++ src/server/controllers/reviews_controller.js
@@ -0,0 +1,17 @@
+const Review = require('../models/review');
+
+exports.getReviews = function(req, res, next) {
+
+}
+
+exports.getWineReviews = function(req, res, next) {
+
+}
+
+exports.getUserReviews = function(req, res, next) {
+
+}
+
+exports.createReview = function(req, res, next) {
+
+}
