73a749fc62fa35df830a303f12790b5870382508
Yondy Kang <yokangy@gmail.com>
2016-07-23 16:21:14 -0700

    Added routes for adding reviews and getting reviews for a specific wine.

--- src/server/routes/reviews_routes.js
+++ src/server/routes/reviews_routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const reviewRouter = express.Router();
+const Reviews = require('../controllers/reviews_controller');
 
 module.exports = function(app) {
-  reviewRouter.get('/wine/:id');
+  reviewRouter.get('/wine/:id', Reviews.getWineReviews);
   reviewRouter.get('/user/:id');
   reviewRouter.get('/:id');
-  reviewRouter.post('/');
+  reviewRouter.post('/', Reviews.addReview);
 
   app.use('/reviews', reviewRouter);
 }
