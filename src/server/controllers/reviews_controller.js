120b7d48cc9d5660b7264f850b8f276376c4ff4b
Yondy Kang <yokangy@gmail.com>
2016-07-23 16:22:25 -0700

    Added methods for adding reviews to mongo db and retrieving reviews for a wine from mongodb.

--- src/server/controllers/reviews_controller.js
+++ src/server/controllers/reviews_controller.js
@@ -1,11 +1,22 @@
 const Review = require('../models/review');
+const Wine = require('../models/wine');
 
 exports.getReviews = function(req, res, next) {
 
 }
 
 exports.getWineReviews = function(req, res, next) {
-
+  const wineID = req.params.id;
+console.log(wineID)
+  Review.find({ wine: wineID }, function(err, reviews){
+    if (err) { return next(err); }
+console.log(reviews)
+    if(reviews) {
+      return res.status(201).json(reviews);
+    } else {
+      return res.status(422).send({ error: 'No reviews found'});
+    }
+  })
 }
 
 /*
@@ -14,6 +25,30 @@ exports.getUserReviews = function(req, res, next) {
 }
 */
 
-exports.createReview = function(req, res, next) {
+exports.addReview = function(req, res, next) {
+  const wineID = req.body.wineID;
+  const rating = req.body.rating;
+  const reviewText = req.body.review;
+
+  const review = new Review({
+    rating: rating,
+    review: reviewText,
+    wine: wineID
+  });
+
+  Wine.findOne({ _id: wineID }, function(err, existingWine) {
+    if (err) { return next(err) }
+
+    if(existingWine) {
+      review.save(function(err, review) {
+        if (err) { return next(err) }
+
+        return res.status(201).json(review);
+      })
+    } else {
+      return res.status(422).send({error: 'Wine does not exist'});
+    }
+  })
+
 
 }
